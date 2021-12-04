97d7e6b474edf51bf853b5b56218dec30f4e31eb
kanazawanao <naomiti3333@gmail.com>
2021-12-05 08:22:22 +0900

    add expensesList

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
@@ -12,21 +12,11 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
