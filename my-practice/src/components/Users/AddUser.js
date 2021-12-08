f5f4fd856d8aedf08d11b03a00575b7d7d2d8270
kanazawanao <naomiti3333@gmail.com>
2021-12-08 23:52:41 +0900

    adding validation & resetting logic

--- my-practice/src/components/Users/AddUser.js
+++ my-practice/src/components/Users/AddUser.js
@@ -9,7 +9,15 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   }
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
