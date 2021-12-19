3615358cc69aa9d64b4dc18386dd88b3988ce30d
kanazawanao <naomiti3333@gmail.com>
2021-12-19 10:32:25 +0900

    A closer look at child component re-evalutation

--- 03-starting-setup/src/App.js
+++ 03-starting-setup/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -15,8 +16,8 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
