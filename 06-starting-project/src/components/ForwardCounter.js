9970093c0572499faa7f9302ea4fe62c61f70a43
kanazawanao <naomiti3333@gmail.com>
2021-12-25 21:48:16 +0900

    Configuring Custom hooks

--- 06-starting-project/src/components/ForwardCounter.js
+++ 06-starting-project/src/components/ForwardCounter.js
@@ -1,9 +1,8 @@
 import useCounter from '../hooks/use-counter';
-
 import Card from './Card';
 
 const ForwardCounter = () => {
-  const counter = useCounter();
+  const counter = useCounter(true);
 
   return <Card>{counter}</Card>;
 };
