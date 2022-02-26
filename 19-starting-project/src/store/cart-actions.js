84789d03dd595f01b2464ad01e8f6fac2a800a02
kanazawanao <naomiti3333@gmail.com>
2022-02-26 18:26:06 +0900

    Finalizing the Fetching Logic

--- 19-starting-project/src/store/cart-actions.js
+++ 19-starting-project/src/store/cart-actions.js
@@ -19,7 +19,10 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity,
+      }));
     } catch (error) {
       dispatch(uiActions.showNotification({
         status: 'error',
@@ -44,7 +47,7 @@ export const sendCartData = (cart) => {
         'https://react-http-ef4ba-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({items: cart.items, totalQuantity: cart.totalQuantity}),
         }
       );
       if (!response.ok) {
